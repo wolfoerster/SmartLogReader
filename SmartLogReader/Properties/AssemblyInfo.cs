f469462dac3a2d957923b4204dc5cbdd4b051452
Wolfgang Förster <wolfoerster@gmx.de>
2026-05-29 14:23:32 +0200

    new version 3.1.0

--- SmartLogReader/Properties/AssemblyInfo.cs
+++ SmartLogReader/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyVersion("3.1.0")]
